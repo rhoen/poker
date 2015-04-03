0039d7d448c6de6f0aa50d63f9d7c6e8c7b734a3
Rhoen + Emmanuel <rhoen+emmanuel@appacademy.io>
2015-04-03 16:44:11 -0400

    Finish deck speck and class. Update card class with == method.

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,4 +12,21 @@ class Deck
     cards
   end
 
+
+  attr_reader :cards
+
+  def initialize(cards = self.class.all_cards)
+    @cards = cards
+  end
+
+  def deal(num)
+    cards.shift(num)
+  end
+
+  def add_cards(new_cards)
+    cards.concat(new_cards)
+  end
+
+
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,24 +2,28 @@ require 'deck'
 
 describe Deck do
   let(:deck) {Deck.new}
+  let(:cards) {[ Card.new(:spades, :ace) ]}
 
   describe "::all_cards" do
     it "has 52 cards" do
       expect(Deck.all_cards.count).to eq(52)
     end
 
-    it  "has no duplicate cards" 
-      # dedup = Deck.all_cards.
-
+    it  "has no duplicate cards" do
+      expect(Deck.all_cards.uniq.count).to eq(52)
+    end
 
   end
 
   describe "#initialize" do
-    it "it creates a new deck with 52 cards by default"
+    subject(:deck2) {Deck.new(cards)}
+    it "it creates a new deck with 52 cards by default" do
+      expect(deck.cards.count).to eq(52)
+    end
 
-
-
-    it  "can be created with an array of cards"
+    it "can be created with an array of cards" do
+      expect(deck2.cards).to eq(cards)
+    end
 
 
 
@@ -28,12 +32,15 @@ describe Deck do
 
 
   describe "#deal" do
-    it "deals given number of cards"
+    it "deals given number of cards" do
+      expect(deck.deal(5).count).to eq(5)
+    end
 
 
-
-
-    it "removes dealt cards from the deck"
+    it "removes dealt cards from the deck" do
+      deck.deal(2)
+      expect(deck.cards.count).to eq(50)
+    end
 
 
 
@@ -41,7 +48,10 @@ describe Deck do
 
   describe "#add_cards" do
 
-    it "returns cards to the bottom of the deck"
+    it "returns cards to the bottom of the deck" do
+      deck.add_cards(cards)
+      expect(deck.cards.count).to eq(53)
+    end
 
 
 
