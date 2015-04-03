1c9e2b2489643e7b1eef98bfb0b7f6f85056dc6f
Rhoen + Emmanuel <rhoen+emmanuel@appacademy.io>
2015-04-03 14:56:09 -0400

    Initialize card with suit and value.

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,32 @@
 class Card
+  SUIT_STRINGS = {
+    :clubs    => "♣",
+    :diamonds => "♦",
+    :hearts   => "♥",
+    :spades   => "♠"
+  }
+
+  VALUE_STRINGS = {
+    :deuce => "2",
+    :three => "3",
+    :four  => "4",
+    :five  => "5",
+    :six   => "6",
+    :seven => "7",
+    :eight => "8",
+    :nine  => "9",
+    :ten   => "10",
+    :jack  => "J",
+    :queen => "Q",
+    :king  => "K",
+    :ace   => "A"
+  }
+
+  attr_reader :suit, :value
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,14 @@
 require "card"
 
 describe Card do
+  let(:card) {Card.new(:spades, :ace)}
 
   describe "#initialize" do
     it "creates a new card with suit and value" do
-
+      expect(card.suit).to eq(:spades)
+      expect(card.value).to eq(:ace)
     end
 
-    it "returns the correct suit and value when asked" do
-
-    end
 
 
 
