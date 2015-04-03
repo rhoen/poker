a2660f9badb19969c381ba32ae40943f76e72ef1
Rhoen + Emmanuel <rhoen+emmanuel@appacademy.io>
2015-04-03 15:20:05 -0400

    Write spec skeleton for deck class

---
+++ spec/deck_spec.rb
@@ -0,0 +1,58 @@
+require 'deck'
+
+describe Deck do
+  let(:deck) {Deck.new}
+
+  describe "::all_cards" do
+    it "has 52 cards" do
+
+    end
+
+    it  "has no duplicate cards" do
+
+
+    end
+
+  end
+
+
+
+
+  describe "#initialize" do
+    it "it creates a new deck with 52 cards by default" do
+
+    end
+
+    it  "can be created with an array of cards" do
+
+
+    end
+
+  end
+
+
+  describe "#deal" do
+    it "deals given number of cards" do
+
+
+    end
+
+    it "removes dealt cards from the deck" do
+
+    end
+
+  end
+
+  describe "#add_cards" do
+
+    it "returns cards to the bottom of the deck" do
+
+    end
+
+    it 
+
+  end
+
+
+
+end
