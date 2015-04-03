7af4d86174d82d6132d2b770a9cee92c397b2876
Rhoen + Emmanuel <rhoen+emmanuel@appacademy.io>
2015-04-03 14:48:56 -0400

    Make rspec skeleton for card class

---
+++ spec/card_spec.rb
@@ -0,0 +1,18 @@
+require "card"
+
+describe Card do
+
+  describe "#initialize" do
+    it "creates a new card with suit and value" do
+
+    end
+
+    it "returns the correct suit and value when asked" do
+
+    end
+
+
+
+  end
+
+end
